07bd9253fbcb9f4a79e575ca98642794dfb89084
Han <11535773@qq.com>
2023-11-19 00:45:41 +0800

    completed hw1

--- hw1/synthesizer/GuitarString.java
+++ hw1/synthesizer/GuitarString.java
@@ -1,6 +1,5 @@
 package synthesizer;
 
-//Make sure this class is public
 public class GuitarString {
     /** Constants. Do not change. In case you're curious, the keyword final means
      * the values cannot be changed at runtime. We'll discuss this and other topics
